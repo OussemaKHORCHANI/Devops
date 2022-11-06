286fc159d5ca8bcde9c848911abddd7ad2100c70
Oussema <oussema.khorchani@esprit.tn>
2022-11-06 22:54:00 +0100

    Dockerfile update

--- src/main/java/tn/esprit/rh/achat/entities/Operateur.java
+++ src/main/java/tn/esprit/rh/achat/entities/Operateur.java
@@ -1,26 +1,18 @@
 package tn.esprit.rh.achat.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Set;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Operateur implements Serializable{
 	private static final long serialVersionUID = 1L;
 
--- src/main/java/tn/esprit/rh/achat/util/SpringFoxSwaggerConfig.java
+++ src/main/java/tn/esprit/rh/achat/util/SpringFoxSwaggerConfig.java
@@ -1,8 +1,5 @@
 package tn.esprit.rh.achat.util;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -16,6 +13,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 
 @Configuration
@@ -31,15 +32,15 @@ public class SpringFoxSwaggerConfig {
 				.securityContexts(Collections.singletonList(securityContext()))
 				.securitySchemes(Arrays.asList(apiKey()))
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.esprit.examen.controller"))
+				.apis(RequestHandlerSelectors.basePackage("tn.esprit.rh.achat.controllers"))
 				.paths(PathSelectors.any())
 				.build();
 	}
 	
     private ApiInfo apiEndPointsInfo() {
         return new ApiInfoBuilder()
-                .title("My STOCK PROJECT")
-                .description("Micro-Service Documentation")
+                .title("Achat")
+                .description("Devops Project Documentation")
                 .version("1.0.0")
                 .build();
     }
